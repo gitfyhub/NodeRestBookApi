c05a088897863620c902f708628618fde728e856
GitFyHub <Younus@mcbp3621.local>
2016-12-24 23:58:45 -0600

    Added a new route to create a new book

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
 	mongoose = require('mongoose');
+	bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -9,9 +10,20 @@ var app =  express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+	.post(function(req, res){
+		var book = new Book(req.body);
+
+		//console.log(book);
+		book.save();
+		res.status(201).send(book);
+
+	})
 	.get(function(req, res){
 
 		var query = {};
